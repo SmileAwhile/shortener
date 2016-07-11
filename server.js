b40d9273492e5dbd1de5099c5e4050ac912dba96
SmileAwhile <cjdunn11@gmail.com>
2016-07-11 12:40:59 -0700

    ins test

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express')
 var app = express();
+var url = process.env.MONGOLAB_URI;
+var firstName = "Corey";
+var lastName = "Dunn";
+var mongo = require('mongodb').MongoClient
 
 app.get('/', function (req, res) {
 
-    res.end("HELLO");
+  var doc = {
+    firstName: firstName,
+    lastName: lastName
+  }
+
+  mongo.connect(url, function(err, db) {
+    var collection = db.collection('docs');
+    collection.insert(doc, function(err, data) {
+      console.log(JSON.stringify(doc));
+      db.close();
+    });
+
+  });
 
 });
 
