ffb42b24fa4f87007d0f37880caee92d0c90d368
SmileAwhile <cjdunn11@gmail.com>
2016-07-11 14:38:22 -0700

    close to live

--- server.js
+++ server.js
@@ -3,25 +3,44 @@ var app = express();
 var url = process.env.MONGOLAB_URI;
 var firstName = "Corey";
 var lastName = "Dunn";
-var mongo = require('mongodb').MongoClient
+var mongo = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var col = 'shortened'
 
-app.get('/', function (req, res) {
+app.get('/:short', function (req, res) {
+  var shorter = req.params.short;
 
-  var doc = {
-    firstName: firstName,
-    lastName: lastName
+  mongo.connect(url, function(err, db) {
+    var collection = db.collection(col);
+    collection.findOne({
+      _id: ObjectID(shorter)
+    }, function(err, rv) {
+      res.redirect(rv.original);
+    }); // end findone()
+  }) // end mongo.connect()
+}); // end app.get(/:short)
+
+app.get("/new/:link(*)", function(req, res) {
+  var arg = req.param('link');
+  var id = new ObjectID();
+
+  var entry = {
+    _id: id,
+    original: arg,
+    shorter: "https://shorterer.herokuapp.com/" + id
   }
 
   mongo.connect(url, function(err, db) {
-    var collection = db.collection('docs');
-    collection.insert(doc, function(err, data) {
-      console.log(JSON.stringify(doc));
+    var collection = db.collection(col);
+    collection.insert(entry, function(err, data) {
+      console.log(JSON.stringify(entry));
       db.close();
-    });
+    }); // end insert()
+  }); // end mongo.connect()
+  res.end(JSON.stringify(entry, null, '  '));
+})  // end app.get(/new/:link)
 
-  });
-
-});
+app.use(express.static('public'));
 
 var port = process.env.PORT || 3000;
 app.listen(port);
